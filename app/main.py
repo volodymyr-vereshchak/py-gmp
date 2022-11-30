a0b7305632b0d9e2ac44aa6e2ef64239618ca4f8
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-11-30 20:56:32 +0200

    del token

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ def write_csv_file(
 
 async def main():
     headers = {
-        "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ2b2xvZHlteXIudmVyZXNoY2hhayIsImV4cCI6MTY2OTkwMzIyNCwicm9sZXMiOlsiUk9MRV9ETC1aUC1BTEwtVVNFUlMiLCJST0xFX0RMLUhPLUdNUC1NRVRST0xPR0lTVFMiLCJST0xFX0RMLUhPLUdNUC1ESVNQQVRDSEVSUyJdfQ.INTg9vfFPTAVmOLm2mtkOIS7im4rb14LXpo4romS0ovnW17JR2BNte6CFIx1-PAP6KgCD-ZbL5Inho4ggwErHQ",
+        "Authorization": ""
     }
     
     async with httpx.AsyncClient(headers=headers) as client:
